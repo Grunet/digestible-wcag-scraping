a6459cf7d71851770b4acd95a77566682641d3dd
Grunet <sadikshahidain@gmail.com>
2020-05-30 18:33:35 -0500

    Updating script with new file name

--- save.ts
+++ save.ts
@@ -2,7 +2,7 @@ async function __saveFormattedData(formattedData: any) {
   const content = JSON.stringify(formattedData, null, 4);
 
   Deno.mkdir("dist", { recursive: true });
-  await Deno.writeTextFile("./dist/output.json", content);
+  await Deno.writeTextFile("./dist/wcag22.json", content);
 }
 
 async function saveFormattedData(formattedData: any) {
