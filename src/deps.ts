6f835a744842724ebd2850122ad168143b1747a7
Grunet <sadikshahidain@gmail.com>
2020-08-29 16:56:48 -0500

    Extracting out the cheerio dependency into a shared location

---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+// This typing can be switched to an updated version once https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34036 is fixed
+// @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/deab75bde42b5a82aeb951f5a2edaa09922853f4/types/cheerio/index.d.ts"
+import cheerio from "https://dev.jspm.io/cheerio@1.0.0-rc.3";
+
+export { cheerio };
--- src/mainDocument/scrape.ts
+++ src/mainDocument/scrape.ts
@@ -1,6 +1,4 @@
-// This typing can be switched to an updated version once https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34036 is fixed
-// @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/deab75bde42b5a82aeb951f5a2edaa09922853f4/types/cheerio/index.d.ts"
-import cheerio from "https://dev.jspm.io/cheerio@1.0.0-rc.3";
+import { cheerio } from "../deps.ts";
 
 async function getScrapedData(): Promise<IScrapedData> {
   const res = await fetch("https://www.w3.org/TR/2020/WD-WCAG22-20200227/");
