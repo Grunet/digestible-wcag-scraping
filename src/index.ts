a0932c5094ebc2da6b8f6ea4b76be7ec0c41a141
Grunet <sadikshahidain@gmail.com>
2020-08-30 11:55:13 -0500

    Adding in the scraped example content into the output json for all the success criteria that have them

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
-import { getData } from "./mainDocument/output.ts";
+import { traverse } from "./deps.ts";
+import { getData as getDataFromMainDocument } from "./mainDocument/output.ts";
+import { getData as getDataFromUnderstandingPage } from "./understandingPages/output.ts";
 import { saveData } from "./save.ts";
 
 async function createJsonOutput(pathToSaveDir: string) {
-  const mainDocumentData = await getData();
-  await saveData(pathToSaveDir, mainDocumentData);
+  const mainDocumentData = await getDataFromMainDocument();
+
+  const understandingDataPromises: Promise<any>[] = [];
+
+  traverse(mainDocumentData).forEach(function (
+    this: traverse.TraverseContext,
+    _value: any,
+  ) {
+    if (this?.node?.hasOwnProperty("understand")) {
+      const successCritId = this.parent?.node["id"];
+      const understandPageURL = this.node["understand"];
+
+      understandingDataPromises.push(
+        (async function () {
+          const understandingData = await getDataFromUnderstandingPage(
+            { url: understandPageURL },
+          );
+
+          return [successCritId, understandingData];
+        })(),
+      );
+    }
+  });
+
+  const understandingDataById = Object.fromEntries(
+    await Promise.all(understandingDataPromises),
+  );
+
+  const allCombinedData = mainDocumentData;
+
+  traverse(allCombinedData).forEach(function (
+    this: traverse.TraverseContext,
+    _value: any,
+  ) {
+    if (this?.node?.hasOwnProperty("id")) {
+      const id = this.node["id"];
+
+      if (id in understandingDataById) {
+        const { examples } = understandingDataById[id];
+
+        this.node["examples"] = examples ?? [];
+      }
+    }
+  });
+
+  await saveData(pathToSaveDir, allCombinedData);
 }
 
 export { createJsonOutput };
